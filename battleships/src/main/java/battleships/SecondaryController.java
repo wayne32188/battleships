7efda7d5184d927d10419fb4d6a9b384f0062fa1
wayne <wayne32188@gmail.com>
2025-03-14 15:40:29 +0100

    battleships-4: Added button to switch back to first scene

--- battleships/src/main/java/battleships/SecondaryController.java
+++ battleships/src/main/java/battleships/SecondaryController.java
@@ -1,6 +1,17 @@
 package battleships;
 
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class SecondaryController {
 
+    @FXML
+    Button Button1 = new Button();
+
+    @FXML
+    void buttonHandler() throws IOException {
+        Main.setRoot("ShipPlacement");
+    }
 }
